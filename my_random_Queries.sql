b77d03117e837aa82e0ea798bb010deeb74001dc
Levi's <110788808+Thiago-Levi@users.noreply.github.com>
2024-02-01 16:45:27 -0300

    Update my_random_Queries.sql

--- my_random_Queries.sql
+++ my_random_Queries.sql
@@ -10,3 +10,17 @@ LEFT JOIN
     departments d
 ON
     e.department_id = d.department_id;
+
+SELECT 
+    FIRST_NAME AS NOME,
+    LAST_NAME AS SOBRENOME,
+    FIRST_NAME ||' '||LAST_NAME AS NOME_COMPLETO
+FROM
+    EMPLOYEES;
+
+SELECT 
+    FIRST_NAME AS NOME,
+    LAST_NAME AS SOBRENOME,
+    FIRST_NAME ||' '||LAST_NAME ||', Salário: $' || SALARY AS INFO
+FROM
+    EMPLOYEES;
